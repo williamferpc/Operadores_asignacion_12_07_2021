ee7899785ab49bd509f8f112bf20bdef9f2ab900
williamferpc <willi_9212@hotmail.com>
2021-07-12 21:28:09 -0500

    operadores de asignacion 12_07_2021

---
+++ main.js
@@ -0,0 +1,98 @@
+alert(`Oprime F12 para ver la consola`);
+console.log(`Asignación`);
+// Asignación
+x = 3;
+y = 5;
+console.log(`X => ${x}, Y => ${y}`);
+
+// Asignación de adición
+console.log(`Asignación de adición X = ${x}, Y = ${y}`);
+x++;
+console.log(`X++ => ${x}`);
+x += y;
+console.log(`X + Y => ${x}`);
+x = x + y;
+console.log(`X = X + Y => ${x}`);
+
+// Asignación de sustraccion
+console.log(`Asignación de sustraccion X = ${x}, Y = ${y}`);
+x--;
+console.log(`X-- => ${x}`);
+x -= y;
+console.log(`X -= Y => ${x}`);
+x = x - y;
+console.log(`X = X - Y => ${x}`);
+
+// Asignación de multiplicación
+console.log(`Asignación de multiplicación X = ${x}, Y = ${y}`);
+x *= y;
+console.log(`X *= Y => ${x}`);
+x = x * y;
+console.log(`X = X * Y => ${x}`);
+
+// Asignación de división
+console.log(`Asignación de división X = ${x}, Y = ${y}`);
+x /= y;
+console.log(`X /= Y => ${x}`);
+x = x / y;
+console.log(`X = X / Y => ${x}`);
+
+// Asignación de resto
+console.log(`Asignación de resto X = ${x}, Y = ${y}`);
+x %= y;
+console.log(`X %= Y => ${x}`);
+x = x % y;
+console.log(`X = X % Y => ${x}`);
+
+// Asignación de exponenciación
+console.log(`Asignación de exponenciación X = ${x}, Y = ${y}`);
+x **= y;
+console.log(`X **= Y => ${x}`);
+x = x ** y;
+console.log(`X = X ** Y => ${x}`);
+
+// Asignación de desplazamiento a la izaquierda
+console.log(`Asignación de desplazamiento a la izaquierda X = ${x}, Y = ${y}`);
+x <<= y;
+console.log(`X <<= Y => ${x}`);
+x = x << y;
+console.log(`X = X << Y => ${x}`);
+
+// Asignación de desplazamiento a la derecha
+console.log(`Asignación de desplazamiento a la derecha X = ${x}, Y = ${y}`);
+x >>= y;
+console.log(`X >>= Y => ${x}`);
+x = x >> y;
+console.log(`X = X >> Y => ${x}`);
+
+// Asignación sin signo de desplazamiento a la derecha
+console.log(`Asignación sin signo de desplazamiento a la derecha X = ${x}, Y = ${y}`);
+x >>>= y;
+console.log(`X >>>= Y => ${x}`);
+x = x >>> y;
+console.log(`X = X >>> Y => ${x}`);
+
+// Asignación AND
+console.log(`Asignación AND X = ${x}, Y = ${y}`);
+x &= y;
+console.log(`X &= Y => ${x}`);
+x = x & y;
+console.log(`X = X & Y => ${x}`);
+
+// Asignación XOR
+console.log(`Asignación XOR X = ${x}, Y = ${y}`);
+x ^= y;
+console.log(`X ^= Y => ${x}`);
+x = x ^ y;
+console.log(`X = X ^ Y => ${x}`);
+
+// Asignación OR
+console.log(`Asignación OR X = ${x}, Y = ${y}`);
+x |= y;
+console.log(`X |= Y => ${x}`);
+x = x | y;
+console.log(`X = X | Y => ${x}`);
+
+
+
+
